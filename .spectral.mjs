79b9c8abbc8b92133629aa874423dd4442035810
Pranjal Dubey <pranjaldub1999@gmail.com>
2023-05-19 14:43:58 +0530

    Added 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwMTY1Nw/spectral.js?branch_slug=master&token=fcaacf62-188c-41e4-b60c-57e2a18372f9";
+export default { extends: ruleset };

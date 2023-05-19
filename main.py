a44ce9ab9604adeb90e3454c27c80e4cae32604c
pranjaldub <pranjaldub1999git@gmail.com>
2023-05-19 13:06:40 +0530

    fastapi scanqa

---
+++ main.py
@@ -0,0 +1,41 @@
+from transformers import pipeline
+from fastapi import FastAPI 
+from typing import Union
+import pytesseract
+
+
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/answerFromText/{{question}:{context}}")
+def read_item(question , context):
+    nlp_qa = pipeline("question-answering")
+    return nlp_qa({
+    'question': question,
+    'context': context
+})
+
+@app.get("/answerFromImage/{{image}:{question}}")
+async def read_item( image,question ):
+   
+  
+    nlp = pipeline(
+    "document-question-answering",
+    model="impira/layoutlm-document-qa",
+    )
+
+    return nlp(image,question)
+
+@app.get("/summarize/{{article}:{maxLength}}")
+async def summarize( article,maxLength=130 ):
+   
+  
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+    return summarizer(article, max_length=maxLength, min_length=30, do_sample=False)
